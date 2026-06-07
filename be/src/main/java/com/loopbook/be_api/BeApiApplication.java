727f20cc733acafa713ca457a4a3c0c414d0dfff
Lê Anh Tuấn <22130310@st.hcmuaf.edu.vn>
2026-06-07 18:58:52 +0700

    init be

---
+++ be/src/main/java/com/loopbook/be_api/BeApiApplication.java
@@ -0,0 +1,13 @@
+package com.loopbook.be_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApiApplication.class, args);
+	}
+
+}
