ef0cfd54d509a0dd24a016f4907eaa82af881418
22130056-HoHoangDuy <22130056@st.hcmuaf.edu.vn>
2026-06-27 14:47:03 +0700

    Administrators review posts selling books

--- be/src/main/java/com/loopbook/be_api/repositories/BookRepository.java
+++ be/src/main/java/com/loopbook/be_api/repositories/BookRepository.java
@@ -28,4 +28,6 @@ public interface BookRepository extends JpaRepository<Book, String> {
     Page<Book> findAll(Pageable pageable);
     
     Optional<Book> findByIdAndSellerId(String bookId, UUID sellerId);
+
+    List<Book> findByStatus(String status);
 }
